f7bbecf0420b1cb4fa798059ef7676ad33cfa3dc
Maria Celdran Noguera <m.celdrannoguera@um.es>
2026-03-14 17:18:00 +0100

    Todas las clases creadas. A falta de algunos métodos

--- Sistema.py
+++ Sistema.py
@@ -5,13 +5,13 @@
 from enum import Enum
 from abc import ABCMeta, abstractmethod
 
-# Creamos Primero las Enumeraciones presentes en el diagrama UML con el módulo enum
+# ------------------------------ 
 
+# Creamos Primero las Enumeraciones presentes en el diagrama UML con el módulo enum
 class EClase(Enum):
     EJECUTOR = 0
     ECLIPSE = 1
     SOBERANO = 2
-
 
 
 class EUbicacion(Enum):
@@ -20,8 +20,10 @@ class EUbicacion(Enum):
     NEBULOSA_KALIIDA = 2
 
 
-# Creamos la clase abstracta UnidadCombate con el módulo abc
+# ------------------------------ 
 
+
+# Creamos la clase abstracta UnidadCombate con el módulo abc
 
 class UnidadCombate(metaclass=ABCMeta):
 
@@ -39,8 +41,7 @@ class UnidadCombate(metaclass=ABCMeta):
     def get_repuestos(self):
         pass
 
-
-
+# ------------------------------ 
 
 # Creammos las distintas clases del esquema
 
@@ -50,7 +51,6 @@ class Repuesto():
         self.proveedor = proveedor
         self._numero = numero  # Nos pide explícitamente el enunciado que pongamos este atributo como privado
         self.precio = precio
-
     
     def __str__(self):
         return f"Nombre: {self.nombre}; Proveedor: {self.proveedor}, Numero: {self._numero}, Precio: {self.precio}"
@@ -59,9 +59,8 @@ class Repuesto():
     def mostrar_informacion(self):
         print(self)
 
-
+# ------------------------------ 
     
-
 '''
 
 ¿Implementación?
@@ -87,9 +86,7 @@ class TripuPasaje():
         self.tripulacion = tripulacion
         self.pasaje = pasaje
 
-
-    
-
+# ------------------------------ 
 
 
 class Nave(UnidadCombate):
@@ -98,39 +95,146 @@ class Nave(UnidadCombate):
         self.nombre = nombre
         self.piezas_repuesto = []
     
-
     def consultar_repuesto(self, nombre:str):
         for repuesto in self.piezas_repuesto:
             if repuesto.nombre == nombre:
                 return True
         return False
     
-
     def mostrar_informacion(self):
-        print(f"Id_combate: {self.id_combate}; Num_cod: {self.num_cod}; Nombre: {self.nombre}")
+        print(f"Id_combate: {self.id_combate}; Num_cod: {self._num_cod}; Nombre: {self.nombre}")
     
     def get_repuestos(self):
+        # Muestra los nombres de piezas que este tipo de nave puede usar
         for i in self.piezas_repuesto:
             print(i + '\n')
 
     # Añadir aquí anyadir_repuestos() ?
 
+'''
+class Nave(UnidadCombate):
+    def __init__(self, id_combate: str, num_cod: int, nombre: str):
+        super().__init__(id_combate, num_cod)
+        self.nombre = nombre
+        self.piezas_repuesto = []
+    
+    def consultar_repuesto(self, nombre:str):
+        busqueda = nombre.lower().strip()
+        for repuesto in self.piezas_repuesto:
+            if repuesto.nombre.lower().strip() == busqueda:
+                return True
+        return False
+    
+    def mostrar_informacion(self):
+        print(f"Id_combate: {self.id_combate}; Num_cod: {self._num_cod}; Nombre: {self.nombre}")
+    
+    def get_repuestos(self):
+        for i in self.piezas_repuesto:
+            print(i + '\n')
+'''
 
 
+# ------------------------------ 
 
 class EstacionEspacial(TripuPasaje, Nave):
 
     def __init__(self, id_combate: str, num_cod: int, nombre: str, tripulacion: int, pasaje : int, ubicacion: EUbicacion):
-        TripuPasaje.__init__(tripulacion, pasaje)
-        Nave.__init__(id_combate, num_cod, nombre)
+        TripuPasaje.__init__(self,tripulacion, pasaje)
+        Nave.__init__(self,id_combate, num_cod, nombre)
         self.ubicacion = ubicacion
 
     def mostrar_informacion(self):
-        print(f"Id_combate: {self.id_combate}; Num_cod: {self.num_cod}; Nombre: {self.nombre}")
+        print(f"Id_combate: {self.id_combate} -- Num_cod: {self._num_cod} -- Nombre: {self.nombre}")
+
+# ------------------------------ 
+
+class NaveEstelar(TripuPasaje, Nave):
+    
+    def __init__(self, id_combate: str, num_cod: int, nombre: str, tripulacion: int, pasaje : int, clase: EClase):
+        TripuPasaje.__init__(self,tripulacion, pasaje)
+        Nave.__init__(self,id_combate, num_cod, nombre)
+        self.clase = clase
+    
+    def mostrar_informacion(self):
+        print(f'd_combate: {self.id_combate} -- Num_cod: {self._num_cod} -- Nombre: {self.nombre}')
+        
+
+# ------------------------------ 
+
+class CazaEstelar(Nave):
+   
+    def __init__(self, id_combate: str, num_cod: int, nombre: str, dotacion: int):
+        super().__init__(id_combate, num_cod, nombre)
+        self.dotacion = dotacion
+    
+    def mostrar_informacion(self):
+        print(f'd_combate: {self.id_combate} -- Num_cod: {self._num_cod} -- Nombre: {self.nombre}')
+        
+
+# ------------------------------ 
+
+class Almacen():
+    
+    def __init__(self, nombre: str, localizacion: str, catalogo: list):
+        self.nombre = nombre
+        self.localizacion = localizacion
+        self.catalogo = []
+    
+    def comprobar_stock(self, nombre_repuesto): #devuelve si hay stock de ese repuesto que consulta
+        for repuesto in self.catalogo:
+            if repuesto.nombre == nombre_repuesto and repuesto._numero > 0:
+                return True
+        return False
+    
+    def contar_existencias(self): #contar las existencias del almacen de todos los repuestos
+        total = 0
+        for repuesto in self.catalogo:
+            total += repuesto._numero
+        return total
+    
+    def actualizar(self, nombre_repuesto: str, cantidad: int):
+        for repuesto in self.catalogo:
+            if repuesto.nombre == nombre_repuesto:
+                repuesto._numero += cantidad
 
 
+# ------------------------------         
 
-
-hola = EClase.ECLIPSE
-
-print(hola)
+class FlotaEspacial():
+    
+    def __init__(self, ud_combate_imperial:list, almacenes: list):
+        self.ud_combate_imperial = []
+        self.almacenes = []
+    
+    def adquirir_repuesto(self, nave:str, nombre_repuesto:str, cantidad:int, almacenes: list):
+        # vemos si esa nave tiene ese repuesto
+        if not nave.consultar_repuesto(nombre_repuesto):
+            print(f'La nave {nave} no usa el repuesto: {nombre_repuesto}')
+            return []
+        
+        repuestos_adquiridos = []
+        cantidad_solicitada = cantidad
+        
+        # si lo tiene, comprobamos en los almacenes los stocks
+        for almacen in almacenes:
+            stock_disp = almacen.comprobar_stock(nombre_repuesto)
+            if True: #esto se complica. Posible cambio a que comprobar_stock() devuelva la cantidad?
+                
+             
+             
+ 
+# ------------------------------        
+if __name__ == "__main__": 
+    hola = EClase.ECLIPSE
+    print(hola)
+    almacen_maria = Almacen('Almacen Maria', 'Marte', [])
+    rep1 = Repuesto('Tornillo oro','Marias', 100, 200000)
+    almacen_maria.catalogo.append(rep1)
+    print(f'Almacen: {almacen_maria.nombre} -> Existencias: {almacen_maria.contar_existencias()}')
+    
+    nav1 = NaveEstelar("MCN-2005", 123, 'MCN', 10, 5, 'ECLIPSE' )
+    nav1.mostrar_informacion()
+    
+    nav1.piezas_repuesto.append(rep1)
+    print(f'¿Quedan tornillos de oro?: {nav1.consultar_repuesto('Tornillo oro')}')
+    print(f'¿Quedan tornillos de cristal?: {nav1.consultar_repuesto('Tornillo cristal')}')
