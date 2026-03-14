6973ce95e27fa2aeb0da2836c3a7d89506a08d1a
Maria Celdran Noguera <m.celdrannoguera@um.es>
2026-03-14 19:02:23 +0100

    Final operario

--- Sistema.py
+++ Sistema.py
@@ -179,12 +179,16 @@ class Almacen():
         self.nombre = nombre
         self.localizacion = localizacion
         self.catalogo = []
+        
+    def dar_de_alta(self, nombre:str, proveedor:str, cantidad:int, precio:float):
+        nuevo = Repuesto(nombre, proveedor, cantidad, precio)
+        self.catalogo.append(nuevo)
     
     def comprobar_stock(self, nombre_repuesto): #devuelve si hay stock de ese repuesto que consulta
         for repuesto in self.catalogo:
             if repuesto.nombre == nombre_repuesto and repuesto._numero > 0:
-                return True
-        return False
+                return True,repuesto._numero
+        return False,0
     
     def contar_existencias(self): #contar las existencias del almacen de todos los repuestos
         total = 0
@@ -193,19 +197,33 @@ class Almacen():
         return total
     
     def actualizar(self, nombre_repuesto: str, cantidad: int):
-        for repuesto in self.catalogo:
-            if repuesto.nombre == nombre_repuesto:
-                repuesto._numero += cantidad
+        # funcion para mantener el stock. Cantidad positiva añadir, cantidad negativa eliminar. Si llega a 0, eliminamos
+        # como catalogo es una lista,necesitamos el indice para borrar cuando llegue a 0
+        for i, repuesto in enumerate(self.catalogo): 
+            if repuesto.nombre.lower() == nombre_repuesto.lower():
+                if repuesto._numero + cantidad < 0: #no puede haber stock negativo
+                    raise ValueError(f'Stock insuficiente')
+                repuesto._numero += cantidad   
+                if repuesto._numero == 0:
+                    self.catalogo.pop(i)
+                else:
+                    print(f'Stock actualizado')
+                return
+        raise ValueError("Repuesto no encontrado. Hay que darlo de alta en el sistema.")  # decimos esto para que cada vez que un operario quiera actualizar no tenga que meter toda la información del repuesto
 
+    def obtener_catalogo(self):
+        # para la funcionalidad de listar_catalgo del operario
+        return self.catalogo
+    
 
 # ------------------------------         
 
 class FlotaEspacial():
     
     def __init__(self, ud_combate_imperial:list, almacenes: list):
-        self.ud_combate_imperial = []
-        self.almacenes = []
-    
+        self.ud_combate_imperial = ud_combate_imperial
+        self.almacenes = almacenes
+    '''
     def adquirir_repuesto(self, nave:str, nombre_repuesto:str, cantidad:int, almacenes: list):
         # vemos si esa nave tiene ese repuesto
         if not nave.consultar_repuesto(nombre_repuesto):
@@ -218,19 +236,55 @@ class FlotaEspacial():
         # si lo tiene, comprobamos en los almacenes los stocks
         for almacen in almacenes:
             stock_disp = almacen.comprobar_stock(nombre_repuesto)
-            if True: #esto se complica. Posible cambio a que comprobar_stock() devuelva la cantidad?
-                
-             
+            if True: #esto se complica. --> cambio a que comprobar_stock() devuelva (bool,cantidad)
+    '''
+    # El operario mantiene y lista stock 
+    def listar_repuestos(self):
+        #listamos el stock de todos repuestos
+        for almacen in self.almacenes:
+            print(f'Repuestos en el Almacén : {almacen.nombre}')
+            catalogo = almacen.obtener_catalogo()
+            if not catalogo:
+                print('El almacén está vacío')
+            else:
+                for repuesto in catalogo:
+                    print(f"- {repuesto}")
+    
+    def actualizar(self,nombre_repuesto: str, cantidad:int):
+        # La Flota manda la tarea a almacen.
+        for almacen in self.almacenes:
+            try:
+                almacen.actualizar(nombre_repuesto, cantidad)
+                return
+            except ValueError:
+                continue
+        # si no se encontro en ningun almacen, es porque hay que darlo de alta
+        if cantidad >0:
+            print(f'{nombre_repuesto} no existe aún. Inserte los siguientes datos par darlo de alta:\n')
+            precio = float(input('Precio: '))
+            proveedor = input('Nombre del proveedor: ')
+            # Damos de alta en el primer almacén por defecto 
+            if self.almacenes:
+                self.almacenes[0].dar_de_alta(nombre_repuesto, proveedor, cantidad, precio)     
+            else:
+                print("Error: No hay almacenes disponibles.")
+        else:
+            ValueError('No puedes introducir un nuevo repuesto con cantidad negativa.')
+        
+    
+   
+    
              
  
 # ------------------------------        
 if __name__ == "__main__": 
     hola = EClase.ECLIPSE
     print(hola)
-    almacen_maria = Almacen('Almacen Maria', 'Marte', [])
+    almacen_1= Almacen('Almacen Maria', 'Marte', [])
+    mi_flota= FlotaEspacial([], [almacen_1])
     rep1 = Repuesto('Tornillo oro','Marias', 100, 200000)
-    almacen_maria.catalogo.append(rep1)
-    print(f'Almacen: {almacen_maria.nombre} -> Existencias: {almacen_maria.contar_existencias()}')
+    almacen_1.catalogo.append(rep1)
+    print(f'Almacen: {almacen_1.nombre} -> Existencias: {almacen_1.contar_existencias()}')
     
     nav1 = NaveEstelar("MCN-2005", 123, 'MCN', 10, 5, 'ECLIPSE' )
     nav1.mostrar_informacion()
@@ -238,3 +292,13 @@ if __name__ == "__main__":
     nav1.piezas_repuesto.append(rep1)
     print(f'¿Quedan tornillos de oro?: {nav1.consultar_repuesto('Tornillo oro')}')
     print(f'¿Quedan tornillos de cristal?: {nav1.consultar_repuesto('Tornillo cristal')}')
+    
+    #prueba de actualizacion de stock que existe
+    mi_flota.actualizar('Tornillo oro',13)
+    mi_flota.actualizar('Tornillo oro',-200013)  #vemos si se elimina porque se queda a 0
+    print(f'Vacio?: {len(almacen_1.catalogo)}') #aqui me sale 1, deberia ser 0
+    
+    # dar de alta
+    mi_flota.actualizar("Motor de Salto", 5)
+
+    
