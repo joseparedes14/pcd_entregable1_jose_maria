1890f47197a91533aba26b0fe1e8074a49638df6
JoseParedes <joseeparedes14@gmail.com>
2026-03-22 11:08:39 +0100

    Ultimos comentarios

--- test_Tests.py
+++ test_Tests.py
@@ -11,9 +11,10 @@ import Sistema as Sis
 # Test asociado al comportamiento de dar de alta un almacen
 
 def test_dar_alta():
+
     flota = Sis.FlotaEspacial()
     almacen = Sis.Almacen('A1', 'Marte')
-    flota.anyadir_almacen(almacen)
+    flota.anyadir_almacen(almacen) # Instanciamos una flota y le añadimos un almacen
 
     flota.dar_de_alta('Tornillos de Oro', 'Joses', 100, 200, 'A1')
 
@@ -26,11 +27,12 @@ def test_dar_alta():
 def test_stock_insuficiente():
     flota = Sis.FlotaEspacial()
     almacen = Sis.Almacen("A1", "Marte")
-    flota.anyadir_almacen(almacen)
+    flota.anyadir_almacen(almacen)  # Instanciamos una flota y le añadimos un almacen
+
 
     flota.dar_de_alta("Tornillo", "Prov", 5, 5, "A1")
 
-    with pytest.raises(Sis.ErrorStockInsuficiente):
+    with pytest.raises(Sis.ErrorStockInsuficiente): # Intentamos modificar quitando más unidades de las que hay disponibles
         flota.actualizar("Tornillo", -10)
 
 
@@ -42,14 +44,14 @@ def test_stock_insuficiente():
 def test_consultar_repuesto_nave_no_existente():
     flota = Sis.FlotaEspacial()
     nave = Sis.NaveEstelar('CAE1', 32124, 'Halcón 17', 40, 21, Sis.EClase.SOBERANO)
-    nave.anyadir_catalogo('Tornillos De Diamante', 'Joses', 300, 200)
-    flota.anyadir_nave(nave)
+    nave.anyadir_catalogo('Tornillos De Diamante', 'Joses', 300, 200) 
+    flota.anyadir_nave(nave) # Añadimos a la flota una nave con un repuesto en el catálogo
 
-    with pytest.raises(Sis.ErrorInexistenciaNave):
-        flota.consultar_repuesto('Tornillos de Oro', 'CAE2') # No estará
+    with pytest.raises(Sis.ErrorInexistenciaNave): #  Intentamos acceder a otra nave no registrada en nuestra Flota 
+        flota.consultar_repuesto('Tornillos de Oro', 'CAE2') 
 
 
-# Test que maneja que no
+# Test que maneja que efectivamente devuelva False el programa cuando consultemos un repuesto en una nave no compatible con ese repuesto
 def test_repuesto_sin_nave():
     nave = Sis.NaveEstelar('CAE1', 32124, 'Halcón 17', 40, 21, Sis.EClase.SOBERANO)
     nave.anyadir_catalogo('Tornillos De Diamante', 'Joses', 300, 200)
@@ -66,7 +68,7 @@ def test_adquirir_repuesto():
     nave = Sis.NaveEstelar('CAE1', 32124, 'Halcón 17', 40, 21, Sis.EClase.SOBERANO)
     nave.anyadir_catalogo('Tornillos', 'Prov', 0, 200)
     nave.anyadir_catalogo('Tornillos de Oro', 'Prov', 0, 200)
-    flota.anyadir_nave(nave)
+    flota.anyadir_nave(nave) # Añadimos a la flota una nave con dos repuestos en su catalogo
 
 
     almacen = Sis.Almacen("A1", "Marte")
@@ -74,13 +76,13 @@ def test_adquirir_repuesto():
 
     flota.dar_de_alta("Tornillos", "Prov", 10, 200, "A1")
 
-    with pytest.raises(Sis.ErrorStockInsuficiente):
+    with pytest.raises(Sis.ErrorStockInsuficiente): # Comprobamos el error de stock insuficiente
         flota.adquirir_repuesto('Tornillos', 'CAE1', 100)
 
-    with pytest.raises(Sis.ErrorRepuestoNoCatalogo):
+    with pytest.raises(Sis.ErrorRepuestoNoCatalogo): # Compprobamos el error de repuesto incompatible con la nave
         flota.adquirir_repuesto('Tornillo de Oro', 'CAE1', 2)
     
-    with pytest.raises(Sis.ErrorInexistenciaNave):
+    with pytest.raises(Sis.ErrorInexistenciaNave): # Comprobamos el error de nave que no s eencuentra registrada en la nave
         flota.adquirir_repuesto('Tornillos', 'CAE2', 5)
 
     
